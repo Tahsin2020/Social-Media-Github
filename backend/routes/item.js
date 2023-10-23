a2de0ac5ca47bf58b57f48874cf495ef83d1299d
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-10-22 19:24:40 -0700

    Multiple types

--- backend/routes/item.js
+++ backend/routes/item.js
@@ -28,16 +28,16 @@ router.route("/add").post((req, res) => {
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
-  const newExercise = new Exercise({
+  const newItem = new Type({
     username,
     description,
     duration,
     date,
   });
 
-  newExercise
+  newItem
     .save()
-    .then(() => res.json("Exercise added!"))
+    .then(() => res.json("Item added!"))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
