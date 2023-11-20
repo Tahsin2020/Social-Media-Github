3ccff20cb5a0db421efcef8d0784ca5a39f12d24
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-11-20 10:00:21 -0800

    Fixed routes

--- backend/src/routes/public-routes.ts
+++ backend/src/routes/public-routes.ts
@@ -1,22 +1,20 @@
 import { Router } from "express";
-import { verifyToken } from "../utils/token-manager.js";
-import { validate } from "../utils/validators.js";
-
 import {
-  getAllItems,
-  addItem,
-  findItem,
-  modifyItem,
-  deleteItem,
-} from "../controllers/item-controllers.js";
-import { getPublicProfile } from "../controllers/public-controllers.js";
+  getPublicProfile,
+  modifyBio,
+  modifyEducation,
+  modifyExperience,
+  modifyProjects,
+  modifySkills,
+} from "../controllers/public-controllers.js";
 
 const publicRoutes = Router();
 
-publicRoutes.get("/getProfile", getPublicProfile);
-// publicRoutes.post("/modifyProfile", verifyToken, addItem);
-// publicRoutes.get("/getitem", verifyToken, findItem);
-// publicRoutes.get("/modifyitem", verifyToken, modifyItem);
-// publicRoutes.get("/deleteitem", verifyToken, deleteItem);
+publicRoutes.get("/", getPublicProfile);
+publicRoutes.post("/modify/bio", modifyBio);
+publicRoutes.get("/modify/education", modifyEducation);
+publicRoutes.get("/modify/experience", modifyExperience);
+publicRoutes.get("/modify/projects", modifyProjects);
+publicRoutes.get("/modify/skills", modifySkills);
 
 export default publicRoutes;
